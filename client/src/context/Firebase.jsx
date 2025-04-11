caf1d6a64544ca455c8f5a49ee6fe9ed0aa5ad5f
Akhil <akhil.130241@gmail.com>
2025-04-12 03:42:10 +0530

    clean the code

--- client/src/context/Firebase.jsx
+++ client/src/context/Firebase.jsx
@@ -30,13 +30,6 @@ const appId = import.meta.env.VITE_APP_ID
 const databaseURL = import.meta.env.VITE_DATABASE_URL
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyABnpU4cEvnqRZea2gqS7dgNvVS8zPmc7k",
-    // authDomain: "myspace-app-b9054.firebaseapp.com",
-    // projectId: "myspace-app-b9054",
-    // storageBucket: "myspace-app-b9054.firebasestorage.app",
-    // messagingSenderId: "220887697383",
-    // appId: "1:220887697383:web:3b401122a04931dabe1f42",
-    // databaseURL: "https://myspace-app-b9054-default-rtdb.firebaseio.com"
 
      apiKey: apiKey,
     authDomain: authDomain,
